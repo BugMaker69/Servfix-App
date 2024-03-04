6a49a1655d577a0b93ba4adccf6322069a79ad9d
Omar Elsaadany <elsaadanythehunter@gmail.com>
2024-03-04 19:22:25 +0200

    Add Reusable Components

--- app/src/main/java/com/example/graduationproject/ReusableComponents.kt
+++ app/src/main/java/com/example/graduationproject/ReusableComponents.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.graduationproject
 
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -6,27 +8,42 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.graduationproject.ui.theme.DarkBlue
 
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -111,6 +128,88 @@ fun CustomButtonAndText(
         )
     }
 }
+
+
+
+@Composable
+fun CustomTopAppBar(
+    modifier: Modifier = Modifier,
+    title:String,
+    navigationIcon : @Composable () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(containerColor = DarkBlue, actionIconContentColor = Color.White, titleContentColor = Color.White,navigationIconContentColor = Color.White),
+) {
+    TopAppBar(
+        title = { Text(text = title) },
+        colors = colors,
+        actions = actions,
+        navigationIcon = navigationIcon,
+    )
+
+}
+
+
+
+
+@Composable
+fun CustomDialog(
+    modifier: Modifier = Modifier,
+    confirmButtonColors: ButtonColors = ButtonDefaults.buttonColors(containerColor = Color.Transparent, contentColor = Color.Red),
+    dismissButtonColors: ButtonColors = ButtonDefaults.buttonColors(containerColor = Color.Transparent, contentColor = Color.Black),
+    confirmButtonText : String,
+    dismissButtonText : String,
+    onConfirmButtonClick:()->Unit,
+    onDismissButtonClick:()->Unit,
+    dialogTitle:String,
+    dialogText:String,
+) {
+    var show by remember { mutableStateOf(false) }
+    AlertDialog(
+        modifier = modifier,
+        onDismissRequest = { show = false },
+        confirmButton = {
+            Button(onClick = onConfirmButtonClick,colors = confirmButtonColors) {
+                Text(text = confirmButtonText,)
+            }
+        },
+        dismissButton = {
+            Button(onClick = onDismissButtonClick , colors = dismissButtonColors) {
+                Text(text = dismissButtonText)
+            }
+        },
+        title = {
+            Text(
+                text = dialogTitle,
+                textAlign = TextAlign.Center
+            )
+        },
+        text = {
+            Text(
+                text = dialogText,
+                textAlign = TextAlign.Center
+            )
+        }
+    )
+}
+
+
+@Composable
+fun ScreensTemplate(
+    modifier: Modifier = Modifier,
+    topBar: @Composable () -> Unit = {},
+    bottomBar: @Composable () -> Unit = { BottomAppBar(Modifier, { "" }) },
+    content: @Composable (PaddingValues) -> Unit
+) {
+
+    Scaffold(
+        topBar = topBar,
+        bottomBar = bottomBar,
+        content = content
+    )
+
+}
+
+
 
 
 @Composable
